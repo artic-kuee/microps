df7612534ff0f80e7595e3dbeb34045021d4ca15
artic_kuee <artichoke.exe@gmail.com>
2022-03-05 23:02:22 +0900

    step11

--- icmp.c
+++ icmp.c
@@ -1,9 +1,12 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 
 #include "util.h"
 #include "ip.h"
 #include "icmp.h"
+
+#define ICMP_BUFSIZ IP_PAYLOAD_SIZE_MAX
 
 struct icmp_hdr {
     uint8_t type;
@@ -76,6 +79,8 @@ icmp_dump(const uint8_t *data, size_t len)
 #endif
     funlockfile(stderr);
 }
+
+
 void
 icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface)
 {
@@ -98,6 +103,43 @@ icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct
 
     debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(dst, addr2, sizeof(addr2)), len);
     icmp_dump(data,len);
+    switch (hdr->type) {
+    case ICMP_TYPE_ECHO:
+        /* Responds with the address of the received interface. */
+        icmp_output(ICMP_TYPE_ECHOREPLY, hdr->code, ntoh32(hdr->values),data + ICMP_HDR_SIZE, len - ICMP_HDR_SIZE, iface->unicast, src);
+
+        break;
+    default:
+        /* ignore */
+        break;
+    }
+
+
+}
+
+int
+icmp_output(uint8_t type, uint8_t code, uint32_t values, const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst)
+{
+    uint8_t buf[ICMP_BUFSIZ];
+    struct icmp_hdr *hdr;
+    size_t msg_len;
+    char addr1[IP_ADDR_STR_LEN];
+    char addr2[IP_ADDR_STR_LEN];
+
+    hdr = (struct icmp_hdr *)buf;
+    hdr->type = type;
+    hdr->code = code;
+    hdr->sum = 0;
+    hdr->values = hton32(values);
+    for(size_t i = 0; i < len; i++){
+        *(buf + ICMP_HDR_SIZE + i) = data[i];
+    }
+    hdr->sum = cksum16((uint16_t *)hdr, len + ICMP_HDR_SIZE, 0);
+    debugf("%d",cksum16((uint16_t *)hdr, len + ICMP_HDR_SIZE, 0));
+
+    debugf("%s => %s, len=%zu", ip_addr_ntop(src, addr1, sizeof(addr1)), ip_addr_ntop(dst, addr2, sizeof(addr2)), msg_len);
+    icmp_dump((uint8_t *)hdr, msg_len);
+    return ip_output(IP_PROTOCOL_ICMP, buf, len + ICMP_HDR_SIZE, src, dst);
 
 }
 
